14b715c27292afe92109e83e7cfe9666a20f7770
QueensRover <23nn@queensu.ca>
2024-03-11 15:50:03 -0400

    commit changes

--- code_snipets/stop.py
+++ code_snipets/stop.py
@@ -1,12 +1,19 @@
-from turtle import right
 from qset_lib import Rover, AngleReader
 from time import sleep
 import signal
+import math
 
 def main():
     rover = Rover() # this line starts the connection to the rover and gives access to the rover data
     angle_reader = AngleReader()
-    rover.send_command(0, 0) 
+
+    # This line allows for the KeyboardInterrupt to worl
+    signal.signal(signal.SIGINT, signal.default_int_handler)
+
+    while True:
+        print("STOP THE ROVER")
+        rover.send_command(0, 0)
+        sleep(1)
 
 if __name__ == "__main__":
     main()
